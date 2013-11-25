a2196b41b57a6231a0155974a15751a48b233766
arnevdb <vandenbroeckarne@gmail.com>
2013-11-25 20:56:54 +0100

    2de scherm toegevoegd

---
+++ QuickOrder/src/com/ak/quickorder/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.ak.quickorder;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+public class LoginActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	public void logIn(View view){
+		Intent myIntent = new Intent(this, PlacesActivity.class);
+        startActivity(myIntent);
+	}
+}
---
+++ QuickOrder/src/com/ak/quickorder/PlacesActivity.java
@@ -0,0 +1,12 @@
+package com.ak.quickorder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PlacesActivity extends Activity {
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.places);
+	}
+}
