3d3c2f22439843ba8a83bff9b9cc9e7f16cb2c96
Arne <vandenbroeckarne@gmail.com>
2013-11-25 16:05:10 +0100

    remove shit

---
+++ QuickOrder/src/com/ak/quickorder/Places.java
@@ -0,0 +1,12 @@
+package com.ak.quickorder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Places extends Activity {
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.places);
+	}
+}
