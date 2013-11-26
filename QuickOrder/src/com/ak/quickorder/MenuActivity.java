31e8bb6bc74e89a3b03d76eaba02fdc34a2c2d8c
arnevdb <vandenbroeckarne@gmail.com>
2013-11-26 16:24:10 +0100

    tabs terug toegevoegd

--- QuickOrder/src/com/ak/quickorder/MenuActivity.java
+++ QuickOrder/src/com/ak/quickorder/MenuActivity.java
@@ -2,13 +2,40 @@ package com.ak.quickorder;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
-public class MenuActivity extends Activity{
+public class MenuActivity extends Activity {
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.menu);
-	
+
 	}
-	
+
+	public void onResume() {
+		TabHost host = (TabHost) findViewById(R.id.tab_host);
+		host.setup();
+
+		TabSpec spec = host.newTabSpec("Tab One");
+		spec.setContent(R.id.tab_one_container);
+		spec.setIndicator("Tab One");
+		host.addTab(spec);
+
+		spec = host.newTabSpec("Tab Two");
+		spec.setContent(R.id.tab_two_container);
+		spec.setIndicator("Tab Two");
+		host.addTab(spec);
+
+		spec = host.newTabSpec("Tab Two");
+		spec.setContent(R.id.tab_two_container);
+		spec.setIndicator("Tab Two");
+		host.addTab(spec);
+		spec = host.newTabSpec("Tab Two");
+		spec.setContent(R.id.tab_two_container);
+		spec.setIndicator("Tab Two");
+		host.addTab(spec);
+		super.onResume();
+	}
+
 }
