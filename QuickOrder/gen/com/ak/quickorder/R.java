a1a77974d9ec65e1e34aa39e3f5e91f100543485
arnevdb <vandenbroeckarne@gmail.com>
2013-11-28 20:49:12 +0100

    knop + textviews onderaan menutabs toegevoegd, aanpasbaar make afhankelijk van prijs

--- QuickOrder/gen/com/ak/quickorder/R.java
+++ QuickOrder/gen/com/ak/quickorder/R.java
@@ -24,13 +24,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000d;
+        public static final int action_settings=0x7f080011;
         public static final int button1=0x7f080000;
         public static final int editText1=0x7f080001;
         public static final int editText2=0x7f080002;
         public static final int imageButton1=0x7f08000b;
         public static final int item1=0x7f080003;
-        public static final int list=0x7f08000c;
+        public static final int list=0x7f080010;
         public static final int main=0x7f080008;
         public static final int menulist=0x7f08000a;
         public static final int scrollView1=0x7f080009;
@@ -38,6 +38,10 @@ public final class R {
         public static final int tab_one_container=0x7f080004;
         public static final int tab_three_container=0x7f080006;
         public static final int tab_two_container=0x7f080005;
+        public static final int textView1=0x7f08000c;
+        public static final int textView2=0x7f08000f;
+        public static final int textView3=0x7f08000e;
+        public static final int textView4=0x7f08000d;
     }
     public static final class layout {
         public static final int login=0x7f030000;
